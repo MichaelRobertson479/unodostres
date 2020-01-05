70e3a7dc984ca3ed801997257a83b61e7a8ac532
Michael Robertson <mrobertson00@stuy.edu>
2020-01-05 01:06:08 -0500

    stuff

--- deck.c
+++ deck.c
@@ -20,11 +20,6 @@
     };
 
     struct decks deck;
-    deck.arr = {"R0","R1","R2","R3","R4","R5","R6","R7","R8","R9",
-                 "B0","B1","B2","B3","B4","B5","B6","B7","B8","B9",
-                 "G0","G1","G2","G3","G4","G5","G6","G7","G8","G9",
-                 "Y0","Y1","Y2","Y3","Y4","Y5","Y6","Y7","Y8","Y9", };
-    deck.size = 40;
 // deck
 
 // hand
@@ -34,7 +29,6 @@
     };
 
     struct hands hand;
-    hand.size = 0;
 // hand
 
 char * lastPlayed;
@@ -73,6 +67,16 @@ void play (int i) {
 }
 
 void init() {
+    
+    deck.arr = {"R0","R1","R2","R3","R4","R5","R6","R7","R8","R9",
+                 "B0","B1","B2","B3","B4","B5","B6","B7","B8","B9",
+                 "G0","G1","G2","G3","G4","G5","G6","G7","G8","G9",
+                 "Y0","Y1","Y2","Y3","Y4","Y5","Y6","Y7","Y8","Y9", };
+    deck.size = 40;
+
+    
+    hand.size = 0;
+
     draw();
     draw();
     draw();
