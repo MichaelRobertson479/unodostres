fce2d6ab2c1ae30dcc58ba5d539b38f5571438b1
Michael Robertson <mrobertson00@stuy.edu>
2020-01-08 01:03:23 -0500

    e

--- network.c
+++ network.c
@@ -1,45 +1,105 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <dirent.h>
-#include <sys/shm.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <time.h>
+// headers
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <errno.h>
+    #include <fcntl.h>
+    #include <unistd.h>
+    #include <sys/stat.h>
+    #include <sys/types.h>
+    #include <sys/wait.h>
+    #include <dirent.h>
+    #include <sys/shm.h>
+    #include <sys/ipc.h>
+    #include <sys/sem.h>
+    #include <time.h>
+// headers
 
-#define NUMOFPLAYERS 12345
+// memory
+    #define NUMOFPLAYERS 12345
+    #define PLAYERS 23456
+    #define TURNCOUNT 34567
+    #define PLAYERNUM 45678
+// memory
+
+int myNumber;
+int players;
 
 void init() {
 
-int shmd;
-  int* data;
-  shmd = shmget(NUMOFPLAYERS, sizeof(int), IPC_CREAT | IPC_EXCL | 0644);
+    int inputPlayers;
+    int playerNumber;
+    int numOfPlayers;
+    int* data;
+    numOfPlayers = shmget(NUMOFPLAYERS, sizeof(int), IPC_CREAT | IPC_EXCL | 0644);
 
-if (shmd == -1) {
-printf("Segment already exists\n");
-shmd = shmget(NUMOFPLAYERS, sizeof(int), 0644);
-  data = shmat(shmd, 0, 0);
-printf("Number of players: %d\n", *data);
-}
+    if (numOfPlayers == -1) {
+        printf("Segment already exists\n");
+        numOfPlayers = shmget(NUMOFPLAYERS, sizeof(int), 0644);
+        data = shmat(numOfPlayers, 0, 0);
+        printf("Number of players: %d\n", *data);
 
-else {
-printf("Segment created\n");
-data = shmat(shmd, 0, 0);
-printf("Input number of players: ");
-//fgets(data, sizeof(int), stdin);
-*data = 4;
-}
+        if (*data == 0) {
+            printf("Game is full\n");
+            exit();
+        }
+
+        else {
+            printf("Joining game...\n");
+
+            //decrease "number of players"
+            (*data)--;
+
+            playerNumber = shmget(PLAYERNUM, sizeof(int), 0644);
+            data = shmat(playerNumber, 0, 0);
+
+            //set "myNumber"
+            myNumber = *data;
+
+            //increase "player number"
+            (*data)++;
+        }
+    }
+
+    else {
+        printf("Segment created\n");
+        data = shmat(numOfPlayers, 0, 0);
+        printf("Input number of players: \n");
+        
+        //change to input later on
+        int input = 4;
+
+        //set "number of players"
+        *data = input;
+
+        //decrement "number of players"
+        (*data)--;
+
+        //set local and shared "players"
+        inputPlayers = shmget(PLAYERS, sizeof(int), IPC_CREAT | 0644);
+        data = shmat(inputPlayers, 0, 0);
+        *data = input;
+        players = input;
+
+        //make self player 1
+        myNumber = 1;
+
+        //set up for next player
+        playerNumber = shmget(PLAYERNUM, sizeof(int), IPC_CREAT | 0644);
+        data = shmat(playerNumber, 0, 0);
+        *data = 2;
+
+        //set turn count
+        // code...
+    }
 }
 
 int main() {
 
-init();
+    init();
 
-return 0;
+    printf("You are player %d\n", myNumber);
+    printf("There are %d players in this game\n", players);
+
+    return 0;
 }
